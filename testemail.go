2c76371ed89ed88172aff2dbca5ea7bc8ade3765
oglog <coinop.logan@gmail.com>
2018-08-13 23:47:24 +0000

    add testemail.go

---
+++ testemail.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+type Mail struct {
+	senderId string
+	toId     string
+	subject  string
+	body     string
+}
+
+type SmtpServer struct {
+	host string
+	port string
+}
+
+func (s *SmtpServer) ServerName() string {
+	return s.host + ":" + s.port
+}
+
+func (mail *Mail) BuildMessage() string {
+	message := ""
+	message += fmt.Sprintf("From: %s\r\n", mail.senderId)
+	message += fmt.Sprintf("To: %s\r\n", mail.toId)
+
+	message += fmt.Sprintf("Subject: %s\r\n", mail.subject)
+	message += "\r\n" + mail.body
+
+	return message
+}
+
+func sendTheMail() {
+	mail := Mail{}
+	mail.senderId = "from@email.duh"
+	mail.toId = "to@email.dur"
+	mail.subject = "This is the email subject"
+	mail.body = "Harry Potter and threat to Israel\n\nGood editing!!"
+
+	messageBody := mail.BuildMessage()
+
+	smtpServer := SmtpServer{host: "smtp.gmail.com", port: "465"}
+
+	log.Println(smtpServer.host)
+	//build an auth
+	auth := smtp.PlainAuth("", mail.senderId, "hackmeplease", smtpServer.host)
+
+	// Gmail will reject connection if it's not secure
+	// TLS config
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         smtpServer.host,
+	}
+
+	conn, err := tls.Dial("tcp", smtpServer.ServerName(), tlsconfig)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client, err := smtp.NewClient(conn, smtpServer.host)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// step 1: Use Auth
+	if err = client.Auth(auth); err != nil {
+		log.Panic(err)
+	}
+
+	// step 2: add all from and to
+	if err = client.Mail(mail.senderId); err != nil {
+		log.Panic(err)
+	}
+	if err = client.Rcpt(mail.toId); err != nil {
+		log.Panic(err)
+	}
+
+	// Data
+	w, err := client.Data()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = w.Write([]byte(messageBody))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client.Quit()
+
+	log.Println("Mail sent successfully")
+
+}
